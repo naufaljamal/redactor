732a48652e4875cc38ccffe226d6a4d612f85f37
naufaljamal <72847006+naufaljamal@users.noreply.github.com>
2023-07-10 18:01:17 -0700

    Create redact_exceptions.py

---
+++ redact_exceptions.py
@@ -0,0 +1,14 @@
+class YAMLNotFoundException(Exception):
+    pass
+
+class InvalidFileStructureException(Exception):
+    pass
+
+class BadDataException(Exception):
+    pass
+
+class BadAESKeyException(Exception):
+    pass
+
+class AESKeyMismatch(Exception):
+    pass
